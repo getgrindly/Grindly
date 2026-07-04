4dc6304df33b97be17d2556666d9ab67ff6bf850
Raymond Oyondi <raymondoyondi@gmail.com>
2026-07-03 19:36:12 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import dynamicImport from 'next/dynamic';
-import App from '@/components/App';
 
 const ClientAppWrapper = dynamicImport(() => import('@/components/ClientAppWrapper'), {
   ssr: false,
 });
 
 export default function Home() {
-  return <App />;
+  return <ClientAppWrapper />;
 }
